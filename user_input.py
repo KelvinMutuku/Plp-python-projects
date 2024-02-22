abb7a476698511ba2c3d42af2441a7d7df0dd255
Kelvinmutuku <mutukuk553@gmail.com>
2024-02-22 21:50:58 +0300

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,9 @@
+#user_input.py
+
+name = input("What is your name? ")
+        age = int(input("How old are you? "))
+        location = input("Where do you live? ")
+
+        message = f"Hello {name}, you are {age} years old and live in {location}."
+
+        print(message)
